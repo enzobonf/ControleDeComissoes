abcaba9cc8a481b7bafb39952d102298bb4b35f1
enzobonf <enzobonfx@gmail.com>
2019-12-21 02:50:41 -0300

    Update sendTask.js

--- inc/sendTask.js
+++ inc/sendTask.js
@@ -49,7 +49,7 @@ function sendEmail(){
                 Espero que me pague rápido kkkkkkk <p>        
                 (Email automático enviado no dia ${moment.parseZone().format("DD/MM/YYYY")} às ${moment().tz('America/Bahia').format("HH:mm")})`;
             
-            emailer.sendEmail(`${results.length} Comissões Atrasadas`, emailText, tr, 'enzobonfx@gmail.com').then(result=>{
+            emailer.sendEmail(`${results.length} Comissões Atrasadas`, emailText, tr, 'leandra@golfershoes.com.br').then(result=>{
                 resolve('Email enviado com sucesso!');
             }).catch(err=>{
                 reject(err);
