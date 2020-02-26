46746310d5463ac92354935fd31fd9d018cdf269
enzobonf <enzobonfx@gmail.com>
2020-02-26 17:18:45 -0300

    design fix

--- inc/sendTask.js
+++ inc/sendTask.js
@@ -33,7 +33,7 @@ function getTr(results){
     let tr = `<html>
                 <body>
                     <p>
-                    <table style=border-radius: 5px; font-family: Calibri;' border='1' cellpadding='0' cellspacing='0'>
+                    <table style='border-radius: 5px; font-family: Calibri;' border='1' cellpadding='0' cellspacing='0'>
                         <thead>
                         <tr>
                             <th align='center' valign='middle'>Pedido</th>
