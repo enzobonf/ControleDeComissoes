14eab58399f1439d16b8b47a6f1af0edb3446175
enzobonf <52019749+enzobonf@users.noreply.github.com>
2020-07-16 20:48:21 -0300

    Update sendTask.js

--- inc/sendTask.js
+++ inc/sendTask.js
@@ -36,8 +36,10 @@ function getSituation(situation, receivementDate){
 function getTr(results){
     
     let somaComissoes = 0;
+    let height = results.length * 25 + 3;
+    console.log(height);
     let tr = `<html>
-                <body style="width: 428px; height: 150px;">
+                <body style="width: 428px; height: ${height}px;">
                     <p>
                     <table style='border-radius: 5px; font-family: Calibri;' border='1' cellpadding='0' cellspacing='0'>
                         <thead>
